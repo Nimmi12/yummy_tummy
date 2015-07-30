316749b56f105864e5b13cb6c0ccb74cafb3906d
Nimmi Mathew <nimmi.mathew@ruby-software.com>
2015-07-30 12:00:02 +0530

    changed logic to iterate categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
+
   def index
-    @categories = Category.all	
+    categories = Category.all	
+    splitted_categories_array = categories.each_slice(4).to_a
+    @categories_in_first_page = splitted_categories_array.first
+    @all_remaining_categories_list = splitted_categories_array.reject!{ |i| i == @categories_in_first_page }
   end
+
 end
